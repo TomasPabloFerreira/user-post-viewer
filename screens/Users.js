3bde79cc686258cc8ea9cb3ab45451b0c6d150d1
TomasPabloFerreira <tomasferreira373@gmail.com>
2020-08-07 05:58:15 -0300

    feat(users-screen): create users list

--- screens/Users.js
+++ screens/Users.js
@@ -1,19 +1,48 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View, FlatList } from 'react-native'
+import { ListItem } from '../components/index'
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
+		alignItems: 'flex-start',
+		justifyContent: 'flex-start',
 	},
+	list: {
+		alignSelf: 'stretch'
+	}
 })
+
+const users = [
+	{ id: '1', name: 'Pepito' },
+	{ id: '2', name: 'Juansin' },
+	{ id: '3', name: 'Juansin' },
+	{ id: '4', name: 'Juansin' },
+	{ id: '5', name: 'Juansin' },
+	{ id: '6', name: 'Juansin' },
+	{ id: '7', name: 'Juansin' },
+	{ id: '8', name: 'Juansin' },
+	{ id: '9', name: 'Juansin' },
+]
+
+const renderUser = ({ item }) => {
+	return (
+		<ListItem
+			title={item.name}
+		/>
+	)
+}
 
 export default () => {
 	return (
 		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
+			<FlatList
+				data={users}
+				keyExtractor={x => x.id}
+				renderItem={renderUser}
+				style={styles.list}
+			/>
 		</View>
 	)
 }
