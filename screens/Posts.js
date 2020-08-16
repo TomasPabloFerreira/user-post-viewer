725e8d8957f8fa70d243fbde0216ce13b4da4319
TomasPabloFerreira <tomasferreira373@gmail.com>
2020-08-16 04:43:17 -0300

    feat(posts-screen): create user posts list

--- screens/Posts.js
+++ screens/Posts.js
@@ -1,19 +1,54 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState, useMemo } from 'react'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+import { ListItem } from '../components/index'
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
+		alignItems: 'flex-start',
+		justifyContent: 'flex-start',
 	},
+	list: {
+		alignSelf: 'stretch'
+	}
 })
 
-export default () => {
+export default ({ navigation }) => {
+	const userId = navigation.getParam('user_id')
+	const [loading, setLoading] = useState(true)
+
+	const [posts, setPosts] = useState([])
+	const filterPosts = (posts, userId) => posts.filter(post => post.userId === userId)
+	const userPosts = useMemo(() => filterPosts(posts, userId), [posts, userId])
+
+
+	const fetchPosts = async () => {
+		const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+		const data = await response.json()
+		setPosts(data)
+		setLoading(false)
+	}
+
+	useEffect(() => { fetchPosts() }, [])
+
 	return (
 		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
+			{
+				loading
+					?	<Text>Loading...</Text>
+					:	<FlatList
+							data={userPosts}
+							keyExtractor={x => String(x.id)}
+							renderItem={({ item }) =>
+								<ListItem
+									title={item.title}
+									handlePress={() => {}}
+								/>
+							}
+							style={styles.list}
+						/>
+			}
 		</View>
 	)
 }
